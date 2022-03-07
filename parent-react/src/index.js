f092a6daaeb974e71bec152d2fd709f08139df14
rayhomie <1572801584@qq.com>
2022-03-07 21:22:16 +0800

    feat: 配置基座

--- parent-react/src/index.js
+++ parent-react/src/index.js
@@ -1,17 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Routes, Link, Route } from 'react-router-dom';
+import { registerMicroApps, start } from 'qiankun';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const app = [
+  {
+    name: 'childApp',
+    // 默认会加载这个html，解析里面的js动态地执行脚本（子应用必须支持跨域）
+    entry: '//localhost:3001', // 指定子应用的html地址（后自动分析html的script标签）
+    container: '#childApp', // 指定容器id，将子应用插入其中
+    activeRule: '/childApp', // 激活路径
+  },
+];
+
+registerMicroApps(app); // 注册应用
+start({
+  prefetch: false, // 取消预加载
+}); // 开启
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      {/* 基座中可以放自己的路由 */}
+      <Link to='/'>基座应用App</Link>
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      {/* 应用其他子路由 */}
+      <Link to='/childApp'>childApp</Link>
+      <Routes>
+        <Route exact path='/' element={<App />}></Route>
+        <Route
+          exact
+          path='/childApp'
+          element={<div id='childApp'></div>}
+        ></Route>
+      </Routes>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
